207b16e06a2141f8148f6993aa9ec0dadf02b1c7
mhdemd <mahdi.eamdi@yahoo.com>
2024-02-01 19:25:03 +0330

    add video-09

---
+++ video-09/test_print.py
@@ -0,0 +1,11 @@
+
+first_var = str("hello world")
+# print(first_var.title())
+# print(first_var.upper())
+# print(first_var.lower())
+second_var = int(1)
+third_var = float(3.14)
+
+print(type(first_var))
+print(type(second_var))
+print(type(third_var))
