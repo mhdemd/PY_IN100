24cb0634d08b7a418d94ddf36a8e6c06f5904adb
mhdemd <mahdi.eamdi@yahoo.com>
2024-02-04 14:03:11 +0330

    Edit video-12

--- video-12/test.py
+++ video-12/test.py
@@ -1,4 +1,3 @@
-x = input("Your name:")
-y = input("your age:")
-# print("Your name is", x, "and Your age is", y)
-print(f"Hi {x} you are {y} years old!")
+x = str(input("Your name:"))
+y = int(input("Your age:"))
+print(x, y)
