fc53461c782c7a889e680640783d960d45be54fa
mhdemd <mahdi.eamdi@yahoo.com>
2024-02-15 18:21:31 +0330

    add video-24

---
+++ part 2 - Introductory Python/video-24/test.py
@@ -0,0 +1,16 @@
+my_list = [1, 2, 3, 4]
+first_element = my_list[0]
+last_element = my_list[-1]
+# print(first_element, last_element)
+
+string_list = ['apple', 'orange', 'banana']
+
+empty_list = []
+
+mixed_list = [1, 'hello', 3.14, True, [10, 20, 30]]
+# print(mixed_list[4][-2])
+
+list_01 = [1, 1, 1, 'hello', 'hello']
+# print(list_01[3], list_01[4])
+
+print(len(list_01), len(empty_list), len(mixed_list))
