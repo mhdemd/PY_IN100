8e2f01ff837c95de3c89d566ffe53aea72e4340f
mhdemd <mahdi.eamdi@yahoo.com>
2024-02-15 10:22:44 +0330

    add video-23

---
+++ part 2 - Introductory Python/video-23/test.py
@@ -0,0 +1,11 @@
+my_list = [1, 2, 3, 4]
+first_element = my_list[0]
+last_element = my_list[-1]
+print(first_element, last_element)
+
+string_list = ['apple', 'orange', 'banana']
+
+empty_list = []
+
+mixed_list = [1, 'hello', 3.14, True, [1, 2, 3]]
+print(mixed_list[4])
