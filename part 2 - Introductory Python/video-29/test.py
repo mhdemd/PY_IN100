80fce17e549697b3bf381c444c5ddf3e48ce3865
mhdemd <mahdi.eamdi@yahoo.com>
2024-02-19 09:48:31 +0330

    add video-29

---
+++ part 2 - Introductory Python/video-29/test.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana","cherry","apple"] 
+
+# print(fruits.index("apple"))
+# print(fruits.count("apple"))
+
+# fruits.sort(reverse=True)
+# print(fruits)
+
+my_fruits = fruits.copy()
+print(my_fruits)
