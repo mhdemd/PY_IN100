f884a99b83da728a98bbc3a858d1ad620b3ff718
mhdemd <mahdi.eamdi@yahoo.com>
2024-03-06 11:59:57 +0330

    add video-40

---
+++ part 2 - Introductory Python/video-40-pr-05/test.py
@@ -0,0 +1,43 @@
+people = {
+    "123456789-1" : {
+        "nam" : "Arshia", 
+        "namekhanevadegi" : "Hosseinpour",
+        "sen" : "21",
+        "mahaltavalod" : "Chabahar",
+        "saltavalod" : "1381",
+        "vazn" : "68",
+        "ghad" : "170"
+    },
+    "123456789-2" : {
+        "nam" : "Zahra", 
+        "namekhanevadegi" : "Rezaie",
+        "sen" : "38",
+        "mahaltavalod" : "Shiraz",
+        "saltavalod" : "1364",
+        "vazn" : "85",
+        "ghad" : "180"
+    },  
+    "123456789-3" : {
+        "nam" : "Parsa", 
+        "namekhanevadegi" : "Norouzi",
+        "sen" : "16",
+        "mahaltavalod" : "Tabriz",
+        "saltavalod" : "1386",
+        "vazn" : "79",
+        "ghad" : "160"
+    },
+    "123456789-4" : {
+        "nam" : "Taha", 
+        "namekhanevadegi" : "Mohammadi",
+        "sen" : "6",
+        "mahaltavalod" : "Tehran",
+        "saltavalod" : "1396",
+        "vazn" : "40",
+        "ghad" : "135"
+    },
+}
+
+print(people["123456789-4"]["nam"])
+print(people["123456789-4"]["namekhanevadegi"])
+print(people["123456789-4"]["sen"])
+print(people["123456789-4"]["saltavalod"])
