8c97c1bf91fafad8340a811346841b7eab7db1e5
mhdemd <mahdi.eamdi@yahoo.com>
2024-03-07 13:40:47 +0330

    add video-41

---
+++ part 2 - Introductory Python/video-41/test.py
@@ -0,0 +1,29 @@
+dict = {
+    1: "Python", 
+    2: "Java", 
+    3: "Ruby", 
+    4: "Scala"
+    }
+
+# dict.update({3:"C++"})
+# dict[3] = "C#"
+# print(dict)
+
+# dict.popitem()
+# print(dict)
+
+# dict.pop(3)
+# print(dict)
+
+# print(list(dict.items()))
+
+# print(dict.get(5, "Hello"))
+
+# print(dict.keys())
+# print(dict.values())
+
+# dict2 = dict.copy()
+# print(dict2)
+
+# dict2 = dict.clear()
+# print(dict2)
