bac734f3859886144e1d43cb705b63e2748afb33
mhdemd <mahdi.eamdi@yahoo.com>
2024-04-11 11:58:36 +0330

    add video 54

---
+++ part 2 - Introductory Python/video-52/test.py
@@ -0,0 +1,10 @@
+people = ["Susan", "Saman", "Sara"]
+
+# print("Hi, Susan!")
+# print("Hi, Saman!")
+# print("Hi, Sara!")
+
+length_list = len(people)
+
+for i in range(length_list): # 0, 1, 2
+    print(f"Hi, {people[i]}!")   
